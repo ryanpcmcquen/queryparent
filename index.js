19d94f72c0cca8433ed4931258425c8f8ff8d315
Ryan P.C. McQuen <ryan.q@linux.com>
2016-07-07 11:49:13 -0700

    Return last element instead of first.

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-/*! queryParent.js v1.1.0 by ryanpcmcquen */
+/*! queryParent.js v1.2.0 by ryanpcmcquen */
 /*global module*/
 /*jshint esversion:6*/
 
+const d = document;
+const qu = 'querySelector';
+
 const queryParent = (s, p) => {
   const q = (x) => {
-    return document.querySelector(x);
+    return d[qu](x);
   };
   const qa = (y) => {
-    return document.querySelectorAll(y);
+    return d.[qu + 'All'](y);
   };
   const pa = Array.prototype.slice.call(qa(p));
   (typeof s === 'string') && (s = q(s));
   return pa.filter((n) => {
     return (n.contains(s)) ? n : false;
-  })[0];
+  }).pop();
 };
 
 module.exports = queryParent;
