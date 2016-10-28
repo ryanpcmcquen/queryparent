be73bf37ed9d7138702157bf6f86fe3f04068f33
Ryan P.C. McQuen <ryan.q@linux.com>
2016-10-28 09:05:20 -0700

    Bring back an es5 version.

---
+++ index-es5.js
@@ -0,0 +1,16 @@
+/*! queryParent.js v1.2.1--ES5 by ryanpcmcquen */
+window.queryParent = function (s, p) {
+  var d = document;
+  var qu = 'querySelector';
+  var q = function (x) {
+    return d[qu](x);
+  };
+  var qa = function (y) {
+    return d[qu + 'All'](y);
+  };
+  var pa = Array.prototype.slice.call(qa(p));
+  (typeof s === 'string') && (s = q(s));
+  return pa.filter(function (n) {
+    return (n.contains(s)) ? n : false;
+  }).pop();
+};
